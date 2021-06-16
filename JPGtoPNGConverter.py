8e353e7913c64c39924f798c130655f620c92d87
Oxerene <dipankerv2@gmail.com>
2021-06-16 15:29:58 -0400

    Made some minor changes

--- JPGtoPNGConverter.py
+++ JPGtoPNGConverter.py
@@ -3,7 +3,6 @@ import os
 from PIL import Image 
 
 
-parent_dir = "C:\Dipanker\python learning\ztmLearning\JPGtoPNGConverter.py\\" 
 # Gets 1st and 2nd argument which respresents the getter folder and the destination folder 
 get_folder = sys.argv[1]
 dest_folder = sys.argv[2]
